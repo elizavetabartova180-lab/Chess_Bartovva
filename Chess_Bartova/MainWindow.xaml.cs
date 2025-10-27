070796bd1b4e95e894dbd9cced7553824ca77d14
student-a502 <student-a502@STUD-RM20-A502>
2025-10-27 19:26:27 +0500

    Разработанный класс Pawn. Бартова

--- Chess_Bartova/MainWindow.xaml.cs
+++ Chess_Bartova/MainWindow.xaml.cs
@@ -4,10 +4,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.Generic;
+using Chess_Bartova.Classes;
 
 namespace Chess_Bartova
 {
