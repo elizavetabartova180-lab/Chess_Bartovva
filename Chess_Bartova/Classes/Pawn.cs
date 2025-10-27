b2673930bd6d212393b91c0cf86c66cea4fad98a
student-a502 <student-a502@STUD-RM20-A502>
2025-10-27 20:04:22 +0500

    почти готово

---
+++ Chess_Bartova/Classes/Pawn.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Chess_Bartova.Classes
+{
+    public class Pawn
+    {
+        public int X, Y;
+        public bool Select, Black;
+        public Grid Figure;
+        public Pawn(int x, int y, bool Black)
+        {
+            this.X = x; 
+            this.Y = y; 
+            this.Black = Black;
+        }
+
+        public void SelectFigure(object semder, MouseButtonEventArgs e)
+        {
+            bool atack = false;
+            Pawn SelectPawn = MainWindow.init.Pawns.Find(x => x.Select == true);
+            if (SelectPawn != null)
+            {
+                if (Black && Y - 1 == SelectPawn.Y && (X == SelectPawn.X - 1 || X == SelectPawn.X + 1) ||
+                    !Black && Y + 1 == SelectPawn.Y && (X == SelectPawn.X - 1 || X == SelectPawn.X + 1))
+                {
+                    MainWindow.init.gameBoard.Children.Remove(Figure);
+                    Grid.SetColumn(SelectPawn.Figure, X);
+                    Grid.SetRow(SelectPawn.Figure, Y);
+
+                    SelectPawn.X = X;
+                    SelectPawn.Y = Y;
+
+                    SelectPawn.SelectFigure(null, null);
+                    return;
+                }
+            }
+            MainWindow.init.OneSelect(this);
+            if (Select)
+            {
+                if (Black)
+                    Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn(black).png")));
+                else
+                    Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn.png")));
+                Select = false;
+            }
+            else
+            {
+                Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn(select).png")));
+                Select = true;
+            }
+        }
+
+        public void Transform(int X, int Y)
+        {
+            if(X!= this.X)
+            {
+                SelectFigure(null, null);
+                return ;
+            }
+            if(Black &&((this.Y == 6 && this.Y -2 == Y)|| this.Y - 1 == Y)||
+               !Black && ((this.Y == 1 && this.Y + 2 == Y) || this.Y + 1 == Y))
+            {
+                Grid.SetColumn(Figure, X);
+                Grid.SetRow(Figure, Y);
+
+                this.X = X;
+                this.Y = Y;
+            }
+            SelectFigure(null, null);
+        }
+    }
+}
--- Chess_Bartova/MainWindow.xaml.cs
+++ Chess_Bartova/MainWindow.xaml.cs
@@ -6,6 +6,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Collections.Generic;
 using Chess_Bartova.Classes;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Chess_Bartova
 {
@@ -14,9 +17,45 @@ namespace Chess_Bartova
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Pawn> Pawns = new List<Pawn>();
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+
+            for(int i = 0; i<7; i++)
+            {
+                Pawns.Add(new Pawn(i, 1, false));
+                Pawns.Add(new Pawn(i, 6, false));
+            }
+            CreatFigures();
+        }
+        public void CreatFigures()
+        {
+            foreach (Pawn Pawn in Pawns) {
+                Pawn.Figure = new Grid()
+                {
+                    Width = 50,
+                    Height = 50,
+                };
+                if (Pawn.Black)
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:/Images/Pawn(black).png")));
+                else
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn.png")));
+                Grid.SetColumn(Pawn.Figure, Pawn.X);
+                Grid.SetRow(Pawn.Figure, Pawn.Y);
+                Pawn.Figure.MouseDown += Pawn.SelectFigure;
+                gameBoard.Children.Add(Pawn.Figure);
+            }
+        }
+        public void OneSelect(Pawn pawn)
+        {
+
+        }
+        public void SelectTile(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
